ba53d9d5bcef680a3d9f5f6dd254a820bf106462
Uttkarsh Khanke <uttkarsh.khanke@gmail.com>
2025-10-31 22:35:08 +0530

    Phase 1: Setup completed

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// basic route
+app.get("/", (req, res) => {
+  res.send("EDUFLEX backend is running 🚀");
+});
+
+// connect MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.log("❌ MongoDB error:", err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
